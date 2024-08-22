f5c82b9f084be8f4ddf802b238c260daf026684f
Rafael Mauricio da SIlva <rafael_mauricio@hotmail.com>
2024-08-22 02:59:49 +0000

    adiciona script `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Pra mim mesmo! Continue estudando!!! ESTUDAAAAA!!!</h1>
+  return <h1>Pra mim mesmo! Continue estudando!!! ESTUDAAAAA!!!</h1>;
+}
+
+function teste() {
+  return <h1>arroz </h1>;
 }
 
 export default Home;
