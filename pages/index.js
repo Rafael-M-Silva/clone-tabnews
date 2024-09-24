091b699c19560ed4406f3a2ffe53c74701b4f3a5
Rafael Mauricio da SIlva <rafael_mauricio@hotmail.com>
2024-09-24 19:38:59 +0000

    add `compose` config file with database service

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 function Home() {
-  return <h1>Pra mim mesmo! Continue estudando!!! ESTUDAAAAA!!!</h1>;
+  return (
+    <>
+      <h1>Bigode Ensina</h1>
+      <a href="https://www.youtube.com/@BigodeEnsina" target="_blank">
+        Canal Bigode Ensina
+      </a>
+    </>
+  );
 }
-
 export default Home;
